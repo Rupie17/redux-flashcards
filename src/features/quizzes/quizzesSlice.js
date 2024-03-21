962e22d48814e824109fe113ebf8c462c2c5a740
Rupie17 <johnruperto17@gmail.com>
2024-03-21 08:52:00 -0700

    syncing

--- src/features/quizzes/quizzesSlice.js
+++ src/features/quizzes/quizzesSlice.js
@@ -1,16 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 export const quizzesSlice = createSlice({
-    name: 'quizzes',
-    initialState: {quizzes: {}},
-    reducers: {
-        addQuiz: (state, action) => {
-            const { id } = action.payload;
-            state.quizzes[id] = action.payload;
-        }
-    }
+  name: 'quizzes',
+  initialState: { quizzes: {} },
+  reducers: {
+    addQuiz: (state, action) => {
+      const { id } = action.payload
+      state.quizzes[id] = action.payload
+    },
+  },
 })
 
-export const { addQuiz} = quizzesSlice.actions;
-export const selectQuizzes = state => state.quizzes.quizzes;
-export default quizzesSlice.reducer;
+export const { addQuiz } = quizzesSlice.actions
+export const selectQuizzes = state => state.quizzes.quizzes
+export default quizzesSlice.reducer
